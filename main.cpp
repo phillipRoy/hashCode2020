d5ff61486f7805beaa19bbdbe803556b7f247086
Gaige Ehrenworth <gtr2d2.gc@gmail.com>
2020-02-20 13:07:08 -0600

    added file handling for file specified via command line

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <fstream>
 #include "class.hpp"
 
-int main() {
-  helloWorld myHello;
+//global variables
+int books;
+int libraries;
+int days;
+
+int main(int argc, char **argv[]) {
+  //setup all infile object constants
+  std::ifstream infile;
+  infile.open(argv[1]); //open file specified by command line argument
+
+  //assign file values to variables
+  infile >> books; //number of books
+  infile >> libraries; //number of libraries
+  infile >> days; // number of days until deadline
+
+  //initialization of objects
+
   return 0;
 }
