a6b6fa275ad516dd265248c90e0cfc7548a1a268
Nicholas Stewart <nrstewart@ualr.edu>
2020-02-20 15:14:24 -0600

    push

--- class.hpp
+++ class.hpp
@@ -12,5 +12,18 @@ public:
   //Instance Variables
   std::vector<int> Books;
   int signup;
-  int throughput;
+  int bookIdIndex = 0;
+  int throughput;//output starts here
+  void Library::SendBooks()
+  {
+    std::cout << numberOfBooks << std::endl;
+
+    for(int i = 0; i < throughput && i < Books.size(); ++i)
+    {
+      std::cout << Books[bookIdIndex] << " ";
+      bookIdIndex++;
+    }
+  };
+
+
 };
