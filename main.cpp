6e64edc79acadc28cdab5762dd5514ad6aa42b03
Gaige Ehrenworth <gtr2d2.gc@gmail.com>
2020-02-20 14:48:16 -0600

    working parser proof of concept and library class abstraction

--- main.cpp
+++ main.cpp
@@ -8,44 +8,52 @@ int main(int argc, char *argv[]) {
   std::ifstream infile;
   infile.open(argv[1]); //open file specified by command line argument
 
+  //initialize variables for file read
+  int books;
+  int numberOfLibraries;
+  int days;
+
   //assign file values to variables
   infile >> books; //global number of books
-  infile >> libraries; //number of libraries
+  infile >> numberOfLibraries; //number of libraries
   infile >> days; // number of days until deadline
 
   //creation of global book list
+  std::vector<int> bookScores;
   for(int i = 0; i < books; ++i) {
-    static int tempBookScore;
-    static std::vector<int> bookScores;
+    int tempBookScore;
     infile >> tempBookScore;
     bookScores.push_back(tempBookScore);
   }
 
   //initialization of objects
   std::vector<Library> libraries;
-  for(int i = 0; i < libraries.size(); ++i) {
+  for(int i = 0; i < numberOfLibraries; ++i) {
     //creation of library specific variables
-    static int libraryBooks;
+    int libraryBooks;
     infile >> libraryBooks;
-    static int signUpDuration;
+    int signUpDuration;
     infile >> signUpDuration;
-    static int bookThroughput;
+    int bookThroughput;
     infile >> bookThroughput;
 
-    libraries.push_back(*(new Library(libraryBooks, signUpDuration, bookThroughput)));
+    Library tempLibrary(libraryBooks, signUpDuration, bookThroughput);
+    libraries.push_back(tempLibrary);
     for(int j = 0; j < libraries[i].Books.size(); ++j) {
-      static int tempBook;
+      int tempBook;
       infile >> tempBook;
-      libraries[i].Books.push_back(tempBook);
+      libraries[i].Books[j] = tempBook;
     }
   }
 
   for(int i = 0; i < libraries.size(); ++i) {
     for(int j = 0; j < libraries[i].Books.size(); ++j) {
-      std::cout << libraries[i].Books[j] << std::endl;
+      std::cout << libraries[i].Books[j];
     }
-    std::cout << libraries[i].signup << std::endl;
-    std::cout << libraries[i].throughput << std::endl;
+    std::cout << std::endl;
+    std::cout << libraries[i].signup;
+    std::cout << libraries[i].throughput;
+    std::cout << std::endl;
   }
 
   return 0;
